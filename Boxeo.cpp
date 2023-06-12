1af22541377a25c865c05c44e63593278fffb1ad
nicooreal <nicoreal99@gmail.com>
2023-06-11 22:29:01 -0300

    CLASE BOXEO

---
+++ Boxeo.cpp
@@ -0,0 +1,57 @@
+#include "Boxeo.h"
+
+
+Boxeo::Boxeo(){
+Fecha fecha;
+_fechaAlta = fecha;
+_fechaLimitePago = fecha; // falta funcion para calcular;
+_cuotaMensual =  3000;
+
+}
+
+
+bool Boxeo::getActivo(){
+
+return _activo;
+
+}
+void Boxeo::setActivo(bool activo){
+
+_activo = activo;
+
+}
+
+Fecha Boxeo::getFechaAlta(){
+
+return _fechaAlta;
+
+}
+void Boxeo::setFechaAlta(Fecha fecha){
+
+_fechaAlta = fecha;
+
+}
+
+Fecha Boxeo::getFechaLimitePago(){
+
+return _fechaLimitePago;
+
+}
+void Boxeo::setFechaLimitePago(Fecha fecha){
+
+_fechaLimitePago = fecha;
+
+}
+
+bool Boxeo::getCuotaMensual(){
+
+return _cuotaMensual;
+
+
+}
+void Boxeo::setCuotaMensual(float cuota){
+
+
+_cuotaMensual = cuota;
+
+}
---
+++ Boxeo.h
@@ -0,0 +1,29 @@
+#ifndef BOXEO_H
+#define BOXEO_H
+#include "Fecha.h"
+
+class Boxeo
+{
+    private:
+bool _activo;
+Fecha _fechaAlta;
+Fecha _fechaLimitePago;
+float _cuotaMensual;
+
+    public:
+Boxeo();
+bool getActivo();
+void setActivo(bool activo);
+
+Fecha getFechaAlta();
+void setFechaAlta(Fecha fecha);
+
+Fecha getFechaLimitePago();
+void setFechaLimitePago(Fecha fecha);
+
+bool getCuotaMensual();
+void setCuotaMensual(float cuota);
+
+};
+
+#endif // BOXEO_H
