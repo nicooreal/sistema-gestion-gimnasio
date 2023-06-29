4ccb2af581c5046c2a18f77a32eb309528dc0a56
Alexis <AlexisRojasNA20@gmail.com>
2023-06-29 00:32:31 -0300

    Creo que es la ultima validacion

--- Programa.cpp
+++ Programa.cpp
@@ -10,26 +10,24 @@ void Programa::ejecutar()
     do
     {
         //marco.mostrar_menu();
-        do
-        {
-            system("cls");
-            cout<<"--------------------------"<<endl;
-            cout<<"\tMENU PRINCIPAL"<<endl;
-            cout<<"1 - EMPLEADOS"<<endl;
-            cout<<"2 - CLIENTES"<<endl;
-            cout<<"3 - GIMNASIO"<<endl; /// PODIAMOS INCLUIR CLASES COMO BACKUP Y RESTAURAR BACKUP PARA CADA UNA(Recomiendo ver la ultima clase de labo)
-            cout<<"4 - BACKUPS"<<endl;
-            cout<<"0 - SALIR"<<endl;
-            cout<<"--------------------------"<<endl;
 
-            cout<<"OPCION: ";
-            cin>>opcion;
-     validarDosRangos(opcion,0,4);
+        system("cls");
+        cout<<"--------------------------"<<endl;
+        cout<<"\tMENU PRINCIPAL"<<endl;
+        cout<<"1 - EMPLEADOS"<<endl;
+        cout<<"2 - CLIENTES"<<endl;
+        cout<<"3 - GIMNASIO"<<endl; /// PODIAMOS INCLUIR CLASES COMO BACKUP Y RESTAURAR BACKUP PARA CADA UNA(Recomiendo ver la ultima clase de labo)
+        cout<<"4 - BACKUPS"<<endl;
+        cout<<"0 - SALIR"<<endl;
+        cout<<"--------------------------"<<endl;
+
+        cout<<"OPCION: ";
+        cin>>opcion;
+        validarDosRangos(opcion,0,4);
 
 
-            system("cls");
-        }
-        while(opcion<0||opcion>4);
+        system("cls");
+
 
 
         switch(opcion)
